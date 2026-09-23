6dbaa2b1d45905a557213f283931f5f9d7694df7
guisabellla <guisabella05@hotmail.com>
2026-09-22 22:02:42 -0700

    edit comments

--- tests/test_part2.py
+++ tests/test_part2.py
@@ -1,24 +1,3 @@
-'''
-Test cases for HTML tags that also contain attributes within the tags.
-
-NOTE:
-It is customary to write test cases in a sequence of "simplest to most complicated",
-which allows us to develop our code starting with the simplest features first.
-The HTML in this file is more complicated than the HTML in part1,
-and that's why it is in its own separate file labeled part2.
-
-HINT:
-All the test cases in this file or for the validate_html function.
-But the easiest way to get them to pass is to modify the extract_tags function.
-My solution didn't have to modify validate_html at all.
-I strongly encourage you to add some doctests to the _extract_tags function
-that are related to attributes and get those doctests to pass.
-If you created good doctests, then the more complicated "integration tests"
-in this file will pass "automatically".
-Working on simpler doctests for _extract_tags will be much easier
-than working on the more complicated tests in this file.
-'''
-
 import HTML_Validator
 
 
