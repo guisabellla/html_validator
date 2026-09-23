969b4905198b4acfd58eda8b9fcf99df0ee61ec5
guisabellla <guisabella05@hotmail.com>
2026-09-22 21:58:25 -0700

    test2

--- HTML_Validator.py
+++ HTML_Validator.py
@@ -3,8 +3,6 @@
 
 def validate_html(html):
     '''
-    This function performs a limited version of html validation by checking whether every opening tag has a corresponding closing tag.
-
     >>> validate_html('<strong>example</strong>')
     True
     >>> validate_html('<strong>example')
@@ -42,12 +40,6 @@ def validate_html(html):
 
 def _extract_tags(html):
     '''
-    This is a helper function for `validate_html`.
-    By convention in Python, helper functions that are not meant to be used directly by the user are prefixed with an underscore.
-
-    This function returns a list of all the html tags contained in the input string,
-    stripping out all text not contained within angle brackets.
-
     >>> _extract_tags('Python <strong>rocks</strong>!')
     ['<strong>', '</strong>']
     '''
@@ -61,7 +53,15 @@ def _extract_tags(html):
             if close == -1:
                 raise ValueError('found < without matching >')
 
-            tags.append(text[i:close + 1])
+            tag = text[i + 1:close]
+
+            if tag.startswith('/'):
+                name = tag[1:].split()[0]
+                tags.append('</' + name + '>')
+            else:
+                name = tag.split()[0]
+                tags.append('<' + name + '>')
+
             i = close + 1
         else:
             i += 1
